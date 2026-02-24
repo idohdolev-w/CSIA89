bbb30c7d236e65243f3dfcd81c35f7c8b9b419c2
idohdolev-w <ido.h.dolev@gmail.com>
2026-02-24 09:02:59 -0800

    yes

--- src/Bot1.java
+++ src/Bot1.java
@@ -1,4 +1,30 @@
-import AI;
+import java.util.Random;
 public class Bot1 extends Player{
-    Allin();
+    Random random = new Random();
+    enum actions {
+        CHECK,
+        RAISE,
+        CALL,
+        FOLD
+    }
+    int round = 1;
+
+    public Bot1() {
+        this.name = ("brin wei");
+    }
+
+    public void setMoney(int m) {
+        this.money = m;
+    }
+
+    public void increaseRound() { round++; }
+
+    public actions chooseAction() {
+      // fold if cards are same suit but less than a collective value of 13, or diff suit and collective value less than 9 --> fold
+      // if no hand higher than pair after second river, fold
+      // as time goes on, these selections change and flip, he will no longer fold after multiple times playing together.
+    }
+
+
+
 }
